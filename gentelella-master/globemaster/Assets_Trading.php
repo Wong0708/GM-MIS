08bef0505ad55abe165d3eec2f5f18e52fece067
Wong0708 <kenneth_wong@dlsu.edu.ph>
2019-02-17 16:34:27 +0800

    awaw

--- gentelella-master/globemaster/Assets_Trading.php
+++ gentelella-master/globemaster/Assets_Trading.php
@@ -240,6 +240,8 @@
                 <div class="row x_title">
                   <div class="col-md-6">
                     <h3>Trading Asset <small></small></h3>
+                    <br>
+                    <button>Update List</button>
                   </div>
                   <div class="col-md-6">
                     <div id="reportrange" class="pull-right" style="background: #fff; cursor: pointer; padding: 5px 10px; border: 1px solid #ccc">
@@ -249,12 +251,15 @@
                   </div>
                 
 
-                <div class="col-md-9 col-sm-9 col-xs-12">
-                  <div id="chart_plot_01" height ="500px" width = "500px"></div>
-                </div>
+                  <!-- <div class="x_content">
+                    <canvas id="lineChart"></canvas>
+                  </div> -->
 
-                
-                <div class="col-md-3 col-sm-3 col-xs-12 bg-white"> 
+                  <div class="x_content; col-md-9 col-sm-9 col-xs-12 bg-white">
+                    <canvas id="lineChart" height = "100"></canvas>
+                  </div>
+
+                  <div class="col-md-3 col-sm-3 col-xs-12 bg-white"> 
                       <div class="x_panel tile fixed_height_320">
                         <div class="x_title">
                           <h2>Top Selling This Month: </h2>                          
@@ -499,7 +504,7 @@
     <script src="../vendors/Flot/jquery.flot.pie.js"></script>
     <script src="../vendors/Flot/jquery.flot.time.js"></script>
     <script src="../vendors/Flot/jquery.flot.stack.js"></script>
-    <!--<script src="../vendors/Flot/jquery.flot.resize.js"></script>-->
+    <script src="../vendors/Flot/jquery.flot.resize.js"></script>
     <!-- Flot plugins -->
     <script src="../vendors/flot.orderbars/js/jquery.flot.orderBars.js"></script>
     <script src="../vendors/flot-spline/js/jquery.flot.spline.min.js"></script>
@@ -518,89 +523,270 @@
     <script src="../vendors/echarts/dist/echarts.min.js"></script>
     <script src="../vendors/echarts/map/js/world.js"></script>
 
-    <!-- Custom Theme Scripts 
-    <script src="../build/js/custom.min.js"></script>-->
+    <!-- Custom Theme Scripts -->
+    <!-- <script src="../build/js/custom.js"></script> -->
 
+
+    <?php
+      $months = array("January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December");
+
+    ?>
     <script>
-      	var chart_plot_01_settings = {
-          series: {
-            lines: {
-              show: false,
-              fill: false
-            },
-            splines: {
-              show: true,
-              tension: 0.4,
-              lineWidth: 1,
-              fill: 0.4
-            },
-            points: {
-              radius: 0,
-              show: true
-            },
-            shadowSize: 2
-          },
-          grid: {
-            verticalLines: true,
-            hoverable: true,
-            clickable: true,
-            tickColor: "#d5d5d5",
-            borderWidth: 1,
-            color: '#fff'
-          },
-          colors: ["rgba(38, 185, 154, 0.38)", "rgba(3, 88, 106, 0.38)"],
-          xaxis: {
-            tickColor: "rgba(51, 51, 51, 0.06)",
-            mode: "time",
-            tickSize: [1, "day"],
-            //tickLength: 10,
-            axisLabel: "Date",
-            axisLabelUseCanvas: true,
-            axisLabelFontSizePixels: 12,
-            axisLabelFontFamily: 'Verdana, Arial',
-            axisLabelPadding: 10
-          },
-          yaxis: {
-            ticks: 8,
-            tickColor: "rgba(51, 51, 51, 0.06)",
-          },
-          tooltip: false,
-         
-        }  // Settings of Chart_plot_01;
-        
-        function gd(year, month, day) 
-        {
-          return new Date(year, month - 1, day).getTime();
+    // Line chart
+
+    var expected = <?php echo json_encode($months); ?>;
+			 
+			if ($('#lineChart').length ){	
+			
+      var ctx = document.getElementById("lineChart");
+      var lineChart = new Chart(ctx, {
+      type: 'line',
+      data: {
+        labels: expected,
+        datasets: [{
+        label: "Item 1",
+        backgroundColor: "rgba(38, 185, 154, 0.31)",
+        borderColor: "rgba(38, 185, 154, 0.7)",
+        pointBorderColor: "rgba(38, 185, 154, 0.7)",
+        pointBackgroundColor: "rgba(38, 185, 154, 0.7)",
+        pointHoverBackgroundColor: "#fff",
+        pointHoverBorderColor: "rgba(220,220,220,1)",
+        pointBorderWidth: 1,
+        data: [31, 74, 6, 39, 20, 85, 7]
+        }, {
+        label: "Item 2",
+        backgroundColor: "rgba(3, 88, 106, 0.3)",
+        borderColor: "rgba(3, 88, 106, 0.70)",
+        pointBorderColor: "rgba(3, 88, 106, 0.70)",
+        pointBackgroundColor: "rgba(3, 88, 106, 0.70)",
+        pointHoverBackgroundColor: "#fff",
+        pointHoverBorderColor: "rgba(151,187,205,1)",
+        pointBorderWidth: 1,
+        data: [82, 23, 66, 9, 99, 4, 2]
+        }]
+      },
+      });
+    
+    }
+
+     /* DATERANGEPICKER */
+	   
+        function init_daterangepicker() {
+
+    if( typeof ($.fn.daterangepicker) === 'undefined'){ return; }
+    console.log('init_daterangepicker');
+
+    var cb = function(start, end, label) {
+      console.log(start.toISOString(), end.toISOString(), label);
+      $('#reportrange span').html(start.format('MMMM D, YYYY') + ' - ' + end.format('MMMM D, YYYY'));
+    };
+
+    var optionSet1 = {
+      startDate: moment().subtract(29, 'days'),
+      endDate: moment(),
+      minDate: '01/01/2012',
+      maxDate: moment(),
+      dateLimit: {
+      days: 60
+      },
+      showDropdowns: true,
+      showWeekNumbers: true,
+      timePicker: false,
+      timePickerIncrement: 1,
+      timePicker12Hour: true,
+      ranges: {
+      'Today': [moment(), moment()],
+      'Yesterday': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
+      'Last 7 Days': [moment().subtract(6, 'days'), moment()],
+      'Last 30 Days': [moment().subtract(29, 'days'), moment()],
+      'This Month': [moment().startOf('month'), moment().endOf('month')],
+      'Last Month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
+      },
+      opens: 'left',
+      buttonClasses: ['btn btn-default'],
+      applyClass: 'btn-small btn-primary',
+      cancelClass: 'btn-small',
+      format: 'MM/DD/YYYY',
+      separator: ' to ',
+      locale: {
+      applyLabel: 'Submit',
+      cancelLabel: 'Clear',
+      fromLabel: 'From',
+      toLabel: 'To',
+      customRangeLabel: 'Custom',
+      daysOfWeek: ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa'],
+      monthNames: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+      firstDay: 1
+      }
+    };
+
+    $('#reportrange span').html(moment().subtract(29, 'days').format('MMMM D, YYYY') + ' - ' + moment().format('MMMM D, YYYY'));
+    $('#reportrange').daterangepicker(optionSet1, cb);
+    $('#reportrange').on('show.daterangepicker', function() {
+      console.log("show event fired");
+    });
+    $('#reportrange').on('hide.daterangepicker', function() {
+      console.log("hide event fired");
+    });
+    $('#reportrange').on('apply.daterangepicker', function(ev, picker) {
+      console.log("apply event fired, start/end dates are " + picker.startDate.format('MMMM D, YYYY') + " to " + picker.endDate.format('MMMM D, YYYY'));
+    });
+    $('#reportrange').on('cancel.daterangepicker', function(ev, picker) {
+      console.log("cancel event fired");
+    });
+    $('#options1').click(function() {
+      $('#reportrange').data('daterangepicker').setOptions(optionSet1, cb);
+    });
+    $('#options2').click(function() {
+      $('#reportrange').data('daterangepicker').setOptions(optionSet2, cb);
+    });
+    $('#destroy').click(function() {
+      $('#reportrange').data('daterangepicker').remove();
+    });
+
+    }
+      
+    function init_daterangepicker_right() {
+      
+      if( typeof ($.fn.daterangepicker) === 'undefined'){ return; }
+      console.log('init_daterangepicker_right');
+
+      var cb = function(start, end, label) {
+        console.log(start.toISOString(), end.toISOString(), label);
+        $('#reportrange_right span').html(start.format('MMMM D, YYYY') + ' - ' + end.format('MMMM D, YYYY'));
+      };
+
+      var optionSet1 = {
+        startDate: moment().subtract(29, 'days'),
+        endDate: moment(),
+        minDate: '01/01/2012',
+        maxDate: '12/31/2020',
+        dateLimit: {
+        days: 60
+        },
+        showDropdowns: true,
+        showWeekNumbers: true,
+        timePicker: false,
+        timePickerIncrement: 1,
+        timePicker12Hour: true,
+        ranges: {
+        'Today': [moment(), moment()],
+        'Yesterday': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
+        'Last 7 Days': [moment().subtract(6, 'days'), moment()],
+        'Last 30 Days': [moment().subtract(29, 'days'), moment()],
+        'This Month': [moment().startOf('month'), moment().endOf('month')],
+        'Last Month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
+        },
+        opens: 'right',
+        buttonClasses: ['btn btn-default'],
+        applyClass: 'btn-small btn-primary',
+        cancelClass: 'btn-small',
+        format: 'MM/DD/YYYY',
+        separator: ' to ',
+        locale: {
+        applyLabel: 'Submit',
+        cancelLabel: 'Clear',
+        fromLabel: 'From',
+        toLabel: 'To',
+        customRangeLabel: 'Custom',
+        daysOfWeek: ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa'],
+        monthNames: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+        firstDay: 1
         }
-        var arr_data1 = [
-              [gd(2019, 1, 1), 17],
-              [gd(2019, 1, 2), 74],
-              [gd(2019, 1, 3), 6],
-              [gd(2019, 1, 4), 39],
-              [gd(2019, 1, 5), 20],
-              [gd(2019, 1, 6), 85],
-              [gd(2019, 1, 7), 7]
-            ];
+      };
 
-            var arr_data2 = [
-              [gd(2019, 1, 1), 82],
-              [gd(2019, 1, 2), 23],
-              [gd(2019, 1, 3), 66],
-              [gd(2019, 1, 4), 9],
-              [gd(2019, 1, 5), 119],
-              [gd(2019, 1, 6), 6],
-              [gd(2019, 1, 7), 9]
-            ];
-        
-           if ($("#chart_plot_01").length){
-            console.log('Plot1');
-            
-            $.plot( $("#chart_plot_01"), [ arr_data1, arr_data2 ],  chart_plot_01_settings )
-            ;
-          }
+      $('#reportrange_right span').html(moment().subtract(29, 'days').format('MMMM D, YYYY') + ' - ' + moment().format('MMMM D, YYYY'));
 
-          	
-        </script>
-	
+      $('#reportrange_right').daterangepicker(optionSet1, cb);
+
+      $('#reportrange_right').on('show.daterangepicker', function() {
+        console.log("show event fired");
+      });
+      $('#reportrange_right').on('hide.daterangepicker', function() {
+        console.log("hide event fired");
+      });
+      $('#reportrange_right').on('apply.daterangepicker', function(ev, picker) {
+        console.log("apply event fired, start/end dates are " + picker.startDate.format('MMMM D, YYYY') + " to " + picker.endDate.format('MMMM D, YYYY'));
+      });
+      $('#reportrange_right').on('cancel.daterangepicker', function(ev, picker) {
+        console.log("cancel event fired");
+      });
+
+      $('#options1').click(function() {
+        $('#reportrange_right').data('daterangepicker').setOptions(optionSet1, cb);
+      });
+
+      $('#options2').click(function() {
+        $('#reportrange_right').data('daterangepicker').setOptions(optionSet2, cb);
+      });
+
+      $('#destroy').click(function() {
+        $('#reportrange_right').data('daterangepicker').remove();
+      });
+
+    }
+
+    function init_daterangepicker_single_call() {
+      
+    if( typeof ($.fn.daterangepicker) === 'undefined'){ return; }
+    console.log('init_daterangepicker_single_call');
+    
+    $('#single_cal1').daterangepicker({
+      singleDatePicker: true,
+      singleClasses: "picker_1"
+    }, function(start, end, label) {
+      console.log(start.toISOString(), end.toISOString(), label);
+    });
+    $('#single_cal2').daterangepicker({
+      singleDatePicker: true,
+      singleClasses: "picker_2"
+    }, function(start, end, label) {
+      console.log(start.toISOString(), end.toISOString(), label);
+    });
+    $('#single_cal3').daterangepicker({
+      singleDatePicker: true,
+      singleClasses: "picker_3"
+    }, function(start, end, label) {
+      console.log(start.toISOString(), end.toISOString(), label);
+    });
+    $('#single_cal4').daterangepicker({
+      singleDatePicker: true,
+      singleClasses: "picker_4"
+    }, function(start, end, label) {
+      console.log(start.toISOString(), end.toISOString(), label);
+    });
+
+
+    }
+
+
+    function init_daterangepicker_reservation() {
+      
+    if( typeof ($.fn.daterangepicker) === 'undefined'){ return; }
+    console.log('init_daterangepicker_reservation');
+
+    $('#reservation').daterangepicker(null, function(start, end, label) {
+      console.log(start.toISOString(), end.toISOString(), label);
+    });
+
+    $('#reservation-time').daterangepicker({
+      timePicker: true,
+      timePickerIncrement: 30,
+      locale: {
+      format: 'MM/DD/YYYY h:mm A'
+      }
+    });
+
+    }
+
+    $(document).ready(function() {
+
+      init_daterangepicker();
+      init_daterangepicker_right();
+      init_daterangepicker_single_call();
+      init_daterangepicker_reservation();
+
+    });	
+    </script>             
   </body>
 </html>
