81c49b8337ce82e299e9f2a8df7869f66699479d
Wong0708 <kenneth_wong@dlsu.edu.ph>
2019-03-03 23:07:58 +0800

    added logout

--- gentelella-master/globemaster/nav.php
+++ gentelella-master/globemaster/nav.php
@@ -114,7 +114,7 @@ if(!(isset($_SESSION['usertype']))){
               <a data-toggle="tooltip" data-placement="top" title="Lock">
                 <span class="glyphicon glyphicon-eye-close" aria-hidden="true"></span>
               </a>
-              <a data-toggle="tooltip" data-placement="top" title="Logout" href="login.html">
+              <a data-toggle="tooltip" data-placement="top" title="Logout" href="logout.php">
                 <span class="glyphicon glyphicon-off" aria-hidden="true"></span>
               </a>
             </div>
@@ -145,7 +145,7 @@ if(!(isset($_SESSION['usertype']))){
                       </a>
                     </li>
                     <li><a href="javascript:;">Help</a></li>
-                    <li><a href="login.html"><i class="fa fa-sign-out pull-right"></i> Log Out</a></li>
+                    <li><a href="logout.php"><i class="fa fa-sign-out pull-right"></i> Log Out</a></li>
                   </ul>
                 </li>
 
