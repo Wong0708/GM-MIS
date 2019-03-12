ea0770a798947aa001e18463169f10bf979976a7
Jake Gratil <jake_gratil@dlsu.edu.ph>
2019-03-12 22:40:03 +0800

    Added Order Approval

--- gentelella-master/globemaster/DataFetchers/mysql_connect.php
+++ gentelella-master/globemaster/DataFetchers/mysql_connect.php
@@ -1,10 +1,6 @@
 <?php
-<<<<<<< HEAD
-$dbc=mysqli_connect('127.0.0.1','root','Rane0708!','mydbmiggy');
-=======
-$dbc=mysqli_connect('localhost','root','','gm_db');
->>>>>>> 6930d2522ee1674769e9883e81868d4470c7a697
 
+$dbc=mysqli_connect('localhost','root','','gm_db');
 
 
 if (!$dbc) {
