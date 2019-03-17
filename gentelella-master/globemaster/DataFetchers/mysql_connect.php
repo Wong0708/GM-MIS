099a16b60263db4d5976bb661d63e5c84a64ca31
Kenneth Wong <kenneth_wong@dlsu.edu.ph>
2019-03-17 18:18:32 +0800

    eoqInventory bugfixes

--- gentelella-master/globemaster/DataFetchers/mysql_connect.php
+++ gentelella-master/globemaster/DataFetchers/mysql_connect.php
@@ -1,9 +1,5 @@
 <?php
-<<<<<<< HEAD
 $dbc=mysqli_connect('localhost','root','Rane0708!','mydbmiggy2');
-=======
-$dbc=mysqli_connect('localhost','root','','gm_db');
->>>>>>> e98833ca6cbf7c20a6534fba167594accc356b32
 
 
 
