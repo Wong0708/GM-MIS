c575716a6dcaf216cb9a4f869ca5d05429993ae2
Kenneth Wong <kenneth_wong@dlsu.edu.ph>
2019-03-17 19:44:33 +0800

    asdfasf

--- gentelella-master/globemaster/DataFetchers/mysql_connect.php
+++ gentelella-master/globemaster/DataFetchers/mysql_connect.php
@@ -1,13 +1,5 @@
 <?php
-<<<<<<< HEAD
-<<<<<<< HEAD
 $dbc=mysqli_connect('localhost','root','Rane0708!','mydbmiggy2');
-=======
-$dbc = mysqli_connect('localhost','root','1234','mydb');
->>>>>>> b53d64a84a309bcbc864c73ee65b38cfbe5f615c
-=======
-$dbc=mysqli_connect('localhost','root','','gm_db');
->>>>>>> 3b0b22ee3bf46631a52345ee353eee22801f9a5e
 
 
 if (!$dbc) {
